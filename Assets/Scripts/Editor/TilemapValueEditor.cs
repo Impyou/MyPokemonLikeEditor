3135efbbe970d6586dec30e2f3578d535578a561
Medhi SAM <sammedhi2@hotmail.fr>
2021-10-13 17:15:12 +0200

    Update Tilesystem to allow set block value

--- Assets/Scripts/Editor/TilemapValueEditor.cs
+++ Assets/Scripts/Editor/TilemapValueEditor.cs
@@ -7,10 +7,14 @@ using UnityEngine;
 public class TilemapValueEditor : Editor
 {
     public float sizeBlock = 0.5f;
-
+    int tmp_width, tmp_height;
+    TilemapValue.TileType newType = TilemapValue.TileType.WALL;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+        tmp_width = EditorGUILayout.IntField("Width", tmp_width);
+        tmp_height = EditorGUILayout.IntField("Height", tmp_height);
+
         if(GUILayout.Button("Generate Labels"))
         {
             GenerateLabels();
@@ -20,15 +24,35 @@ public class TilemapValueEditor : Editor
     public void GenerateLabels()
     {
         var tileMapValue = (TilemapValue)target;
-        if(tileMapValue.tileTypes == null)
-        {
-            tileMapValue.tileTypes = new TilemapValue.TileType[tileMapValue.width, tileMapValue.height];
-        }
+
+        tileMapValue.width = tmp_width;
+        tileMapValue.height = tmp_height;
+
+        tileMapValue.tileTypes = new TilemapValue.TileType[tileMapValue.width * tileMapValue.height];
     }
 
     private void OnSceneGUI()
     {
         ShowLabel();
+        
+        /* TODO : Fix issue y position */
+        Vector3 mousePosition = Event.current.mousePosition;
+        mousePosition.y = Camera.current.pixelHeight - mousePosition.y; //Hard fix
+        mousePosition = Camera.current.ScreenToWorldPoint(mousePosition);
+
+        int x, y;
+        x = (int)(mousePosition.x / sizeBlock);
+        y = (int)((mousePosition.y + sizeBlock) / sizeBlock);
+
+        var e = Event.current;
+        if(e.type == EventType.MouseDown && e.button == 0)
+        {
+            Debug.Log("Click");
+            var tileMapValue = (TilemapValue)target;
+            tileMapValue.SetTile(x, y, newType);
+        }
+
+        
     }
 
     void ShowLabel()
@@ -38,13 +62,13 @@ public class TilemapValueEditor : Editor
         if(tileMapValue.tileTypes == null)
             return;
 
-        for (int i = 0; i < tileMapValue.tileTypes.GetLength(0); i++)
+        for (int i = 0; i < tileMapValue.width; i++)
         {
-            for (int j = 0; j < tileMapValue.tileTypes.GetLength(1); j++)
+            for (int j = 0; j < tileMapValue.height; j++)
             {
-                Debug.Log(tileMapValue.tileTypes[i, j]);
-                Handles.Label(new Vector3(i * sizeBlock, j * sizeBlock, 0f), tileMapValue.tileTypes[i, j].ToString());
+                Handles.Label(new Vector3(i * sizeBlock, j * sizeBlock, 0f), tileMapValue.GetTile(i, j).ToString());
             }
         }
     }
+
 }
--- Assets/Scripts/EditorTools/TilemapValue.cs
+++ Assets/Scripts/EditorTools/TilemapValue.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class TilemapValue : MonoBehaviour
 {
     public enum TileType {NONE, WALL};
-    public int width, height;
-    [SerializeField] public TileType[,] tileTypes; 
+
+    [HideInInspector] public int width, height;
+    [HideInInspector] public TileType[] tileTypes;
+
+    public TileType GetTile(int x, int y)
+    {
+        return tileTypes[y * width + x];
+    }
+
+    public void SetTile(int x, int y, TileType value)
+    {
+        tileTypes[y * width + x] = value;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,4 +30,7 @@ public class TilemapValue : MonoBehaviour
     {
         
     }
+
+
+
 }
